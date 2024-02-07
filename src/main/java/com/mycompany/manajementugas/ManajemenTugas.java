f8bd00e6c244191ec268a9cb208b7cfa853ad32a
ox1dized <ikram.syafwan@gmail.com>
2024-02-07 21:28:11 +0700

    first commit

---
+++ src/main/java/com/mycompany/manajementugas/ManajemenTugas.java
@@ -0,0 +1,15 @@
+package com.mycompany.manajementugas;
+import com.mycompany.manajementugas.GUI_MT;
+
+/**
+ *
+ * @author Ikram
+ */
+public class ManajemenTugas {
+    
+    public static void main(String[] args) {
+        GUI_MT gui = new GUI_MT();
+        gui.setVisible(true);
+    }
+    
+}
